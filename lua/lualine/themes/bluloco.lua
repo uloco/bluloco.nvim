de2d7ee18f9e5cbf5a1da5d8d8ef7ac4807b4eb2
Umut Topuzoğlu <uloco@schiefewelt.de>
2022-11-19 19:53:54 +0800

    Decrease lualine fg contrast

--- lua/lualine/themes/bluloco.lua
+++ lua/lualine/themes/bluloco.lua
@@ -1,25 +1,27 @@
 local dark = {
-  bg     = "#282C34",
-  fg     = "#ABB2BF",
-  green  = "#3FC56B",
-  red    = "#FF6480",
-  purple = "#9F7EFE",
-  blue   = "#3691FF",
-  grey3  = "#2d333e",
-  grey7  = "#384252",
-  grey20 = "#4a5a73",
+  bg       = "#282C34",
+  fg       = "#8691A3",
+  normalFG = "#abb2bf",
+  green    = "#3FC56B",
+  red      = "#FF6480",
+  purple   = "#9F7EFE",
+  blue     = "#3691FF",
+  grey3    = "#2d333e",
+  grey7    = "#384252",
+  grey20   = "#4a5a73",
 }
 
 local light = {
-  bg     = "#F9F9F9",
-  fg     = "#383A42",
-  green  = "#23974A",
-  red    = "#D52753",
-  purple = "#823FF1",
-  blue   = "#275FE4",
-  grey3  = "#ededee",
-  grey7  = "#dee1e3",
-  grey20 = "#b9c2c6",
+  bg       = "#F9F9F9",
+  fg       = "#6D7281",
+  normalFG = "#383A42",
+  green    = "#23974A",
+  red      = "#D52753",
+  purple   = "#823FF1",
+  blue     = "#275FE4",
+  grey3    = "#ededee",
+  grey7    = "#dee1e3",
+  grey20   = "#b9c2c6",
 }
 
 local t = dark
@@ -30,7 +32,7 @@ end
 
 return {
   normal = {
-    a = { bg = t.grey20, fg = t.fg },
+    a = { bg = t.grey20, fg = t.normalFG },
     b = { bg = t.grey7, fg = t.fg },
     c = { bg = t.grey3, fg = t.fg }
   },
