0cb1d318b92e7eca82b0df31bb763c17e4472cc3
Umut Topuzoğlu <uloco@schiefewelt.de>
2022-12-03 20:33:25 +0100

    Make transparency configurable

--- colors/bluloco.lua
+++ colors/bluloco.lua
@@ -1,21 +1,19 @@
--- You probably always want to set this in your vim file
--- vim.opt.background = 'dark'
-vim.g.colors_name = 'bluloco'
+local config = require("bluloco").config
+local theme = require('lush_theme.bluloco')
+local lush = require('lush')
 
--- By setting our module to nil, we clear lua's cache,
--- which means the require ahead will *always* occur.
---
--- This isn't strictly required but it can be a useful trick if you are
--- incrementally editing your config a lot and want to be sure your themes
--- changes are being picked up without restarting neovim.
---
--- Note if you're working in on your theme and have :Lushify'd the buffer,
--- your changes will be applied with our without the following line.
---
--- The performance impact of this call can be measured in the hundreds of
--- *nanoseconds* and such could be considered "production safe".
+vim.g.colors_name = 'bluloco'
 package.loaded['lush_theme.bluloco'] = nil
 
--- include our theme file and pass it to lush to apply
-require('lush')(require('lush_theme.bluloco'))
+local transparent = lush(function()
+  return {
+    Normal { bg = "NONE" }, -- normal text
+  }
+end)
 
+
+if (config.transparent == true) then
+  theme = lush.merge({ theme, transparent })
+end
+
+lush(theme)
--- lua/bluloco/init.lua
+++ lua/bluloco/init.lua
@@ -8,26 +8,10 @@ local defaultConfig = {
   transparent = false,
 }
 
-local transparent = lush(function()
-  return {
-    Normal { bg = "NONE" }, -- normal text
-  }
-end)
+M.config = defaultConfig
 
 function M.setup(options)
-  local config = vim.tbl_deep_extend("force", {}, defaultConfig, options or {})
-  vim.g.colors_name = 'bluloco'
-  package.loaded['lush_theme.bluloco'] = nil
-
-  vim.o.background = "light"
-  local bluloco = require('lush_theme.bluloco')
-
-  local theme = bluloco -- add config differences with lush.merge
-
-  if (config.transparent == true) then
-    theme = lush.merge({ bluloco, transparent })
-  end
-  -- require('lush')(theme)
+  M.config = vim.tbl_deep_extend("force", {}, defaultConfig, options or {})
 end
 
 return M
