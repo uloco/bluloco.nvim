fe7858fc79aef20c4c0ab22c2e183bd1e04685ca
Umut Topuzoğlu <uloco@schiefewelt.de>
2022-12-04 10:26:19 +0100

    Use lush extends

--- lua/bluloco/init.lua
+++ lua/bluloco/init.lua
@@ -16,36 +16,29 @@ end
 
 function M.load()
   local theme = require('lush_theme.bluloco')
-
   vim.g.colors_name = 'bluloco'
   package.loaded['lush_theme.bluloco'] = nil
 
-  local transparent = lush(function()
-    return {
-      Normal { theme.Normal, bg = "NONE" }, -- normal text
-      BufferlineFill { bg = "NONE" },
-    }
-  end)
-
-  -- local italics = lush.extends({ theme }).with(function()
-  local italics = lush(function(injected_functions)
-    local sym = injected_functions.sym
-    return {
-      Statement { theme.Statement, gui = "italic" },
-      Boolean { theme.Boolean, gui = "italic" },
-      Comment { theme.Comment, gui = "italic" },
-      sym("@tag.attribute") { theme["@tag.attribute"], gui = "italic" },
-      sym("@annotation") { theme["@annotation"], gui = "italic" },
-    }
-  end)
-
-
   if (M.config.transparent == true) then
-    theme = lush.merge({ theme, transparent })
+    theme = lush.extends({ theme }).with(function()
+      return {
+        Normal { theme.Normal, bg = "NONE" }, -- normal text
+        BufferlineFill { bg = "NONE" },
+      }
+    end)
   end
 
   if (M.config.italics == true) then
-    theme = lush.merge({ theme, italics })
+    local sym = injected_functions.sym
+    theme = lush.extends({ theme }).with(function()
+      return {
+        Statement { theme.Statement, gui = "italic" },
+        Boolean { theme.Boolean, gui = "italic" },
+        Comment { theme.Comment, gui = "italic" },
+        sym("@tag.attribute") { theme["@tag.attribute"], gui = "italic" },
+        sym("@annotation") { theme["@annotation"], gui = "italic" },
+      }
+    end)
   end
 
   lush(theme)
