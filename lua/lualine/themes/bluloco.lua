4c8a4ea3b4b555e0a6b559912b01f756bc5c37f8
Umut Topuzoğlu <uloco@schiefewelt.de>
2026-02-08 23:40:22 +0100

    Fix lualine color change section b

--- lua/lualine/themes/bluloco.lua
+++ lua/lualine/themes/bluloco.lua
@@ -30,39 +30,39 @@ local light = {
 
 local t = dark
 
-if (vim.o.background == "light") then
-  t = light
+if vim.o.background == "light" then
+	t = light
 end
 
 return {
-  normal = {
-    a = { bg = t.blue, fg = t.bg },
-    b = { bg = t.grey7, fg = t.fg },
-    c = { bg = t.grey3, fg = t.fg }
-  },
-  insert = {
-    a = { bg = t.blueDark, fg = t.bg },
-    b = { bg = t.grey7, fg = t.blueDark },
-    c = { bg = t.grey3, fg = t.fg }
-  },
-  visual = {
-    a = { bg = t.purple, fg = t.bg },
-    b = { bg = t.grey7, fg = t.purple },
-    c = { bg = t.grey3, fg = t.fg }
-  },
-  replace = {
-    a = { bg = t.red, fg = t.bg },
-    b = { bg = t.grey7, fg = t.red },
-    c = { bg = t.grey3, fg = t.fg }
-  },
-  command = {
-    a = { bg = t.green, fg = t.bg },
-    b = { bg = t.grey7, fg = t.green },
-    c = { bg = t.grey3, fg = t.fg }
-  },
-  inactive = {
-    a = { bg = t.grey20, fg = t.grey3, },
-    b = { bg = t.grey5, fg = t.grey20 },
-    c = { bg = t.grey5, fg = t.grey20 }
-  }
+	normal = {
+		a = { bg = t.blue, fg = t.bg },
+		b = { bg = t.grey7, fg = t.fg },
+		c = { bg = t.grey3, fg = t.fg },
+	},
+	insert = {
+		a = { bg = t.blueDark, fg = t.bg },
+		b = { bg = t.grey7, fg = t.fg },
+		c = { bg = t.grey3, fg = t.fg },
+	},
+	visual = {
+		a = { bg = t.purple, fg = t.bg },
+		b = { bg = t.grey7, fg = t.fg },
+		c = { bg = t.grey3, fg = t.fg },
+	},
+	replace = {
+		a = { bg = t.red, fg = t.bg },
+		b = { bg = t.grey7, fg = t.fg },
+		c = { bg = t.grey3, fg = t.fg },
+	},
+	command = {
+		a = { bg = t.green, fg = t.bg },
+		b = { bg = t.grey7, fg = t.fg },
+		c = { bg = t.grey3, fg = t.fg },
+	},
+	inactive = {
+		a = { bg = t.grey20, fg = t.grey3 },
+		b = { bg = t.grey5, fg = t.grey20 },
+		c = { bg = t.grey5, fg = t.grey20 },
+	},
 }
